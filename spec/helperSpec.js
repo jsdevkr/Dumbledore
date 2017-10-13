acb2458a87d7e1e0722b1b199bec0df01c3ac85a
Dongwoo Gim <gimdongwoo@gmail.com>
2017-10-13 17:39:37 +0900

    key for travis

--- spec/helperSpec.js
+++ spec/helperSpec.js
@@ -1,16 +1,16 @@
 describe('In helper', function () {
   const stringHandler = require('../lib/helper/stringHandler');
-  const botCommand = require('../lib/helper/botCommand');
+  const slackBot = require('../lib/helper/slackBot');
 
   it('First letter should be capitalized', () => {
     expect(stringHandler.capitalizeFirstLetter('letter')).toBe('Letter');
   });
 
-  it('Any functions should not be `undefined` in botCommand', () => {
-    expect(botCommand.explainSorting).toBeDefined();
-    expect(botCommand.awardPointsCallback).toBeDefined();
-    expect(botCommand.deductPointsCallback).toBeDefined();
-    expect(botCommand.getAllHousePointsCallback).toBeDefined();
-    expect(botCommand.announcePlainString).toBeDefined();
+  it('Any functions should not be `undefined` in slackBot', () => {
+    expect(slackBot.explainSorting).toBeDefined();
+    expect(slackBot.awardPointsCallback).toBeDefined();
+    expect(slackBot.deductPointsCallback).toBeDefined();
+    expect(slackBot.getAllHousePointsCallback).toBeDefined();
+    expect(slackBot.announcePlainString).toBeDefined();
   });
 });
