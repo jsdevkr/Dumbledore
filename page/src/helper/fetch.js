11fec9b3e4a44d10a275548df746104e12725cb2
jongho <ho1234c@gmail.com>
2017-11-05 15:39:53 +0900

    Fixted lint

--- page/src/helper/fetch.js
+++ page/src/helper/fetch.js
@@ -1,7 +1,15 @@
 export function getBot() {
-    // Todo: create logic for get bot
+  // Todo: create logic for get bot
 }
 
 export function getStudent(id) {
-    // Todo: create logic for get bot
+  const students = id;
+  // Todo: create logic for get student
+  return students;
+}
+
+export function createBot(key, password) {
+  const response = { key, password };
+  // Todo: create logic for add bot
+  return response;
 }
