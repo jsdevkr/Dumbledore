d6806bb4c11330a7cf6fa776fe7b0fe605a2a95d
LeeYeEun2 <31236688+LeeYeEun2@users.noreply.github.com>
2017-11-09 20:45:21 +0900

    modal css

--- page/src/components/BotregisterModal.js
+++ page/src/components/BotregisterModal.js
@@ -2,16 +2,6 @@ import React from 'react';
 import { Button, Header, Icon, Modal, Input } from 'semantic-ui-react';
 
 const BotregisterModal = () => (
-<<<<<<< HEAD
-  <Modal id="modal_size" trigger={<div id="botregister-modal">Bot register</div>} closeIcon>
-    <Header icon="user plus" content="Bot-register" />
-    <Modal.Content>
-       Bot-name <Input id="modal_bot_name" type="text" name="name" placeholder="bot-name" />
-      <p />
-       Password <Input id="modal_bot_pw" type="text" name="name" placeholder="password" />
-      <p />
-       Email <Input id="modal_bot_email" type="text" name="name" placeholder="Email" />
-=======
   <Modal id="modal-size" trigger={<div id="botregister-modal">Bot register</div>} closeIcon>
     <Header icon="user plus" content="Bot-register" />
     <Modal.Content>
@@ -20,7 +10,6 @@ const BotregisterModal = () => (
        Password <Input id="bot_pw" type="text" name="name" placeholder="password" />
       <p />
        Email <Input id="bot_email" type="text" name="name" placeholder="Email" />
->>>>>>> d559303c2a49ca797eed19acac0efe1047296991
     </Modal.Content>
     <Modal.Actions>
       <Button color="grey">
--- page/src/components/LoginModal.js
+++ page/src/components/LoginModal.js
@@ -2,21 +2,12 @@ import React from 'react';
 import { Button, Header, Icon, Modal, Input } from 'semantic-ui-react';
 
 const LoginModal = () => (
-<<<<<<< HEAD
-  <Modal id="modal_size" trigger={<div id="login-modal">Sign In</div>} closeIcon>
-    <Header icon="fort awesome" content="LogIn" />
-    <Modal.Content>
-       api-key <Input id="modal_login_key" type="text" name="api-key" placeholder="api key" />
-      <p />
-       password <Input id="modal_login_pw" type="text" name="password" placeholder="password" />
-=======
   <Modal id="modal-size" trigger={<div id="login-modal">Sign In</div>} closeIcon>
     <Header icon="fort awesome" content="LogIn" />
     <Modal.Content>
        api-key <Input id="login_key" type="text" name="api-key" placeholder="api key" />
       <p />
        password <Input id="login_pw" type="text" name="password" placeholder="password" />
->>>>>>> d559303c2a49ca797eed19acac0efe1047296991
     </Modal.Content>
     <Modal.Actions>
 
