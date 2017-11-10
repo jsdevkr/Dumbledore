c0d29afcdc2cd2979dbe7e7a0356589d385ec230
Simhyunjeong <shj2369@naver.com>
2017-11-10 23:33:36 +0900

    Revert "."
    This reverts commit 70dabd36c454991dceed49afaaeff7134ded9c66.

--- page/src/components/NavBar.js
+++ page/src/components/NavBar.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import './NavBar.css';
-import LoginModal from '../components/LoginModal';
-import BotregisterModal from '../components/BotregisterModal';
 
 const NavBar = () => {
   return (
     <div id="navbar">
       Dumbledore
-
-      <div id="botRegister">
-        <BotregisterModal />
-      </div>
-
-      <div id="login">
-        <LoginModal />
-      </div>
-
     </div>
   );
 };
