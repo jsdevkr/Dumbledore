07d95b64c8fd89552f210f9819d7715aa027487f
JONGHO-SURFACEB\jongho <ho1234c@gmail.com>
2017-11-14 14:20:10 +0900

    Fixed code in webpack.config.prod.js

--- page/webpack.config.prod.js
+++ page/webpack.config.prod.js
@@ -64,7 +64,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
         APP_ID: JSON.stringify(process.env.APP_ID || 'myAppId'),
         SERVER_URL: JSON.stringify(process.env.SERVER_URL || 'http://localhost:1337/parse'),
       }
