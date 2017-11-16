d1c0f30785be2ca72f8f715d7c2291150a298678
JONGHO-SURFACEB\jongho <ho1234c@gmail.com>
2017-11-16 14:49:55 +0900

    Changed favicon and main character in frontend

--- page/src/components/Header.js
+++ page/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Statistic, Image } from 'semantic-ui-react';
-import pictogram from '../../static/pictogram.svg';
+import character from '../../static/character.svg';
 import './Header.css';
 
 const Header = () => {
   return (
     <div id="header">
-      <Image src={pictogram} alt="pictogram" centered />
+      <Image src={character} alt="character" centered />
       <Statistic>
         <Statistic.Value text>
           Dumbledore
